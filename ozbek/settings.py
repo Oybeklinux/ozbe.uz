3faf50c0e63d079956819f8aa0fc314b3cbfe1ba
Oybeklinux <Oybeklinux@gmail.com>
2022-06-24 17:27:47 +0500

    host ozbe.uz added

--- ozbek/settings.py
+++ ozbek/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kannas.uz', 'www.kannas.uz', "127.0.0.1"]
+ALLOWED_HOSTS = ['ozbe.uz', 'www.ozbe.uz', "127.0.0.1"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
