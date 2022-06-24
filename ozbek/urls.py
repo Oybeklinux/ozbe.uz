5c8fbb075211600e139e3a3bfe64f4f2a9a2a4ff
Oybeklinux <Oybeklinux@gmail.com>
2022-06-24 17:26:41 +0500

    api url qo'shildi

--- ozbek/urls.py
+++ ozbek/urls.py
@@ -36,11 +36,11 @@ schema_view = get_schema_view(
  )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('product.urls')),
-    path('', include('account.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    path('api/admin/', admin.site.urls),
+    path('api/', include('product.urls')),
+    path('api/', include('account.urls')),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
